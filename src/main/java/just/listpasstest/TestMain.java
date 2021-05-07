1dc6ba8c184d0aaf641914b1391b1227552abf65
kangmin-gi <bming0227@gmail.com>
2021-05-07 23:35:49 +0900

    Heap 삭제

--- src/main/java/just/listpasstest/TestMain.java
+++ src/main/java/just/listpasstest/TestMain.java
@@ -5,16 +5,7 @@ import java.util.List;
 public class TestMain {
 
   public static void main(String[] args) {
-    JustList justList = new JustList();
-    List<Integer> list = justList.getList();
-    System.out.println("#1 list = " + list);
-    list.add(100);
-    list.add(111);
-    System.out.println("#2 list = " + list);
-    List<Integer> list1 = justList.getList();
-    System.out.println("#3 list = " + list1);
-    list.add(222);
-    System.out.println("#4 list = " + list1);
+
 
   }
 }
