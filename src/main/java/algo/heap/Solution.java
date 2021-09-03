c1a4c579d3577b60b5acaced57c63be2ddd55fa3
kangmin-gi <bming0227@gmail.com>
2021-09-03 23:50:47 +0900

    우선순위큐 2번

--- src/main/java/algo/heap/Solution.java
+++ src/main/java/algo/heap/Solution.java
@@ -1,11 +1,70 @@
 package algo.heap;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Solution {
 
   public static void main(String[] args) {
+    Solution solution = new Solution();
+    int[][] jobs = {{0,3},{1,9},{2,6}};
+    int solution1 = solution.solution(jobs);
+    System.out.println("solution1 = " + solution1);
+  }
 
+  public int solution(int[][] jobs) {
+    Queue<Task> priQ = new PriorityQueue<>();
+    Queue<Task> jobQ = new ArrayDeque<>();
+    int nowSec = 0;
+    int totalTime = 0;
+    int totalSize = jobs.length;
+
+    List<Task> collect = Arrays.stream(jobs)
+        .sorted((o1, o2) -> Integer.compare(o1[0], o2[0]))
+        .map(v -> new Task(v[0], v[1]))
+        .collect(Collectors.toList());
+
+    jobQ.addAll(collect);
+
+    while(!jobQ.isEmpty() || !priQ.isEmpty()) {
+      while (!jobQ.isEmpty()) {
+        Task peekJobQ = jobQ.peek();
+        if (nowSec >= peekJobQ.innerTime) {
+          priQ.add(jobQ.remove());
+        } else if (priQ.isEmpty()) {
+          nowSec = peekJobQ.innerTime;
+          priQ.add(jobQ.remove());
+        } else {
+          break;
+        }
+      }
+
+      Task peek = priQ.peek();
+      if(peek != null) {
+        Task remove = priQ.remove();
+        nowSec +=remove.taskTime;
+        totalTime += nowSec - remove.innerTime;
+        remove.doneTime = nowSec;
+      }
+    }
+
+    return totalTime / totalSize;
+  }
+
+  public static class Task implements Comparable<Task> {
+    int innerTime;
+    int taskTime;
+    int doneTime;
+
+    public Task(int innerTime, int taskTime) {
+      this.innerTime = innerTime;
+      this.taskTime = taskTime;
+    }
+
+    @Override
+    public int compareTo(Task o) {
+      return Integer.compare(taskTime, o.taskTime);
+    }
   }
 
 
